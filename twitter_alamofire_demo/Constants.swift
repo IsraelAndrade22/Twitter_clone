a7780d189f60193d056137de6a23ead1e98db96c
Israel Andrade <israel_andrade@Israels-MacBook-Pro.local>
2018-03-03 23:37:26 -0800

    Add boiler code

---
+++ twitter_alamofire_demo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  twitter_alamofire_demo
+//
+//  Created by Israel Andrade on 3/3/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class Constants {
+    static let consumerKey = "LaghwVCyTK8Kgt9Us1d56xVqT"
+    static let consumerSecret = "IEOquAIy6nFRbC4MKAdqpqO10ZF3k0UH8C7VXJTTPpVw1ZYy85"
+}
---
+++ twitter_alamofire_demo/TweetCell.swift
@@ -0,0 +1,34 @@
+//
+//  TweetCell.swift
+//  twitter_alamofire_demo
+//
+//  Created by Charles Hieger on 6/18/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    var tweet: Tweet! {
+        didSet {
+            tweetTextLabel.text = tweet.text
+            //profilePicture.image = tweet.
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
